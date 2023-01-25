a3f29212225f3b086274bed7aa5749af3758637b
Przemek <przemekbilas@gmail.com>
2023-01-25 19:59:36 +0100

    Add Google Maps services to application

--- app/src/main/java/com/example/trafficeventsapp/MapActivity.java
+++ app/src/main/java/com/example/trafficeventsapp/MapActivity.java
@@ -1,14 +1,28 @@
 package com.example.trafficeventsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+    mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
