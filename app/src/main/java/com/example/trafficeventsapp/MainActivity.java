8c2eb1f8f2be91fe39b844ef6f582fe8b0a270b7
Przemek <przemekbilas@gmail.com>
2023-01-25 18:10:16 +0100

    Add animation on startup screen

--- app/src/main/java/com/example/trafficeventsapp/MainActivity.java
+++ app/src/main/java/com/example/trafficeventsapp/MainActivity.java
@@ -6,16 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imVlogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        imVlogo = (ImageView) findViewById(R.id.logo_icon_startup);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_animation);
+        imVlogo.setAnimation(anim);
         Intent homeIntent = new Intent(MainActivity.this, LoginActivity.class);
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -23,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(homeIntent);
                 finish();
             }
-        }, 2000);
+        }, 2700);
 
     }
 }
