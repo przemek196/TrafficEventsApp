16840c8005ead53521619a04186ce13546d7006a
Przemek <przemekbilas@gmail.com>
2023-04-17 17:11:56 +0200

    Add new functions

---
+++ app/src/main/java/com/example/trafficeventsapp/PinTypes.java
@@ -0,0 +1,11 @@
+package com.example.trafficeventsapp;
+
+public class PinTypes {
+    public static final String TYPE_A = "Type A";
+    public static final String TYPE_B = "Type B";
+    public static final String TYPE_C = "Type C";
+
+    public static final int ICON_A = R.drawable.speed_cntrl_ic;
+    public static final int ICON_B = R.drawable.car_cc_ic;
+    public static final int ICON_C = R.drawable.pol_car_ic;
+}
